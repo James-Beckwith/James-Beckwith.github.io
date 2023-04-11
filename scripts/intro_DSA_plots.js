c6652e24b8f3f9cc1b3fb3ea747ef51f29257157
James-Beckwith <py06jab@gmail.com>
2023-04-11 22:56:07 +0100

    fixing label and dash issue in DSA plots

--- scripts/intro_DSA_plots.js
+++ scripts/intro_DSA_plots.js
@@ -1,36 +1,31 @@
 import { makeArr,plotting } from "./utils.js"
 
-var Nsamp = 1000.0
+var Nsamp = 500.0
 var dt = 0.01
 var L = (Nsamp - 1.0) * dt
 var t = makeArr(0,L,Nsamp)
 var f = 1
 var sig = t.map(x => Math.sin(x * 2 * Math.PI * f))
-var sampling_intervals = makeArr(5, 200, 40)
+var sampling_intervals = makeArr(5, 100, 20)
 var ts = []
 var sigs = []
 var labels = []
 var step = 1
-console.log(sampling_intervals)
+var slider_labels = new Array()
 for (var i = 0; i < sampling_intervals.length; i++) {
     ts[i] = new Array()
     sigs[i] = new Array()
     step = sampling_intervals[i]
     labels.push('Measured Signal')
+    slider_labels.push((step*dt).toString())
     for (var j = 0; j < t.length; j=j+step) {
         ts[i].push(t[j])
         sigs[i].push(sig[j])
     }
 }
 
-var slider_labels = new Array()
-for (i = 0; i < sampling_intervals.length; i++) {
-    slider_labels.push((i*dt).toString())
-}
-
 labels[0] = 'Sine wave'
-
-var fig = plotting(sigs, ts, labels, slider_labels, 'Sine Wave Slider', "Sampling interval: ", "ms", 'longdash')
+var fig = plotting(sigs, ts, labels, slider_labels, 'Sine Wave Slider', "Sampling interval: ", "s", true, 'lines+markers', 'longdash')
 var plotElem = document.getElementById('plot1');
 Plotly.newPlot(plotElem, fig, {showSendToCloud: true});
 
@@ -43,6 +38,6 @@ for (var i = 0; i < sampling_intervals.length; i++) {
         }
     }
 
-var fig = plotting(sigs, ts, labels, slider_labels, 'Sine Wave Slider', "Sampling interval: ", "ms", 'longdash')
+var fig = plotting(sigs, ts, labels, slider_labels, 'Sine Wave Slider', "Sampling interval: ", "s", true, 'lines+markers', 'longdash')
 var plotElem = document.getElementById('plot2');
 Plotly.newPlot(plotElem, fig, {showSendToCloud: true});
